924007afbee8840d6d5e6f632df2347ecdc24aef
secreterror <64742606+secreterror@users.noreply.github.com>
2020-05-22 19:26:58 +0530

    dialog box

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:momentum_app/models/custom_card.dart';
 import 'package:momentum_app/models/main_drawer.dart';
 import 'package:momentum_app/services/authentication.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class FeedPage extends StatefulWidget {
   FeedPage({Key key, this.userId, this.auth, this.logoutCallback})
@@ -19,47 +21,96 @@ class _FeedPageState extends State<FeedPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: MainDrawer(logoutCallback: widget.logoutCallback,),
-        floatingActionButton:
-            RaisedButton(onPressed: widget.logoutCallback, color: Colors.blue),
-        body: Container(
-          child: Center(
-            child: Container(
-                padding: const EdgeInsets.all(10.0),
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: Firestore.instance
-                      .collection('userData')
-                      .document(widget.userId)
-                      .collection('userList')
-                      .snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError)
-                      return new Text('Error: ${snapshot.error}');
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return new Text('Loading...');
-                      default:
-                        return new ListView(
-                          children: snapshot.data.documents
-                              .map((DocumentSnapshot document) {
-                            return Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: CustomCard(
-                                title: document['title'],
-                                difficulty: document['difficulty'],
-                                problemId: document['problemId'],
-                                judge: document['judge'],
-                                description: document['description'],
-                                acCallback: () => {},
-                                deleteCallback: () => {},
-                              ),
+        body: Column(
+          children: <Widget>[
+
+            Padding(
+              padding: const EdgeInsets.only(top:100),
+              child: RaisedButton(
+                child: Text('Click'),
+                color: Colors.black,
+                onPressed: (){
+                  return Alert(
+                      context: context,
+                      title: "Problem",
+                      content: Column(
+                        children: <Widget>[
+                          TextField(
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.remove_from_queue),
+                              labelText: 'QuestionId',
+                            ),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.lock),
+                              labelText: 'Judge',
+                            ),
+                          ),
+                        ],
+                      ),
+                      buttons: [
+                        DialogButton(
+                          onPressed: () => {},
+                          child: Text(
+                            "Find",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        )
+                      ]).show();
+                },
+
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: Firestore.instance
+                          .collection('userData')
+                          .document(widget.userId)
+                          .collection('userList')
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasError)
+                          return new Text('Error: ${snapshot.error}');
+                        switch (snapshot.connectionState) {
+                          case ConnectionState.waiting:
+                            return new Text('Loading...');
+                          default:
+                            return new ListView(
+                              shrinkWrap:true,
+                              children: snapshot.data.documents
+                                  .map((DocumentSnapshot document) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: CustomCard(
+                                    title: document['title'],
+                                    difficulty: document['difficulty'],
+                                    problemId: document['problemId'],
+                                    judge: document['judge'],
+                                    description: document['description'],
+                                    acCallback: () => {},
+                                    deleteCallback: () => {},
+                                  ),
+                                );
+                              }).toList(),
                             );
-                          }).toList(),
-                        );
-                    }
-                  },
-                )),
-          ),
-        ));
+                        }
+                      },
+                    )
+                ),
+
+              ),
+            ),
+          ],
+        ),
+      floatingActionButton:
+      RaisedButton(onPressed: widget.logoutCallback, color: Colors.blue,child: Text('Logout'),
+      ),
+
+    );
   }
 }
